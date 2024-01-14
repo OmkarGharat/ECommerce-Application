ca2128c606f728d31d6078c251d5eb638e3871fe
Omkar <omkargharat022@gmail.com>
2024-01-14 13:51:14 +0530

    Adding Movie Categories

--- Data/MovieCategory.cs
+++ Data/MovieCategory.cs
@@ -3,12 +3,14 @@
     public enum MovieCategory
     {
         Action = 1,
-        Romance, 
+        Romance,
+        Cartoon,
         Comedy, 
         Drama, 
         Horror, 
         Mystery, 
         Sports, 
-        Thriller
+        Thriller,
+        Documentary
     }
 }
