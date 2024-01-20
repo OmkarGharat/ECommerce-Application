d780441b157e396bd4dc82e2d3e54371071b2e68
Omkar <omkargharat022@gmail.com>
2024-01-20 19:05:35 +0530

    Adding Actor Service and its interface

---
+++ Data/Services/ActorService.cs
@@ -0,0 +1,43 @@
+﻿using ECommerce_Application.Models;
+using Microsoft.EntityFrameworkCore;
+using ECommerce_Application.Data;
+
+namespace ECommerce_Application.Data.Services
+{
+    public class ActorService : IActorService
+    {
+        private readonly AppDbContext _context;
+
+        public ActorService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Actor actor)
+        {
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var result = await _context.Actors.ToListAsync();
+            return result;
+        }
+
+        public async Task<Actor> GetById(int id)
+        {
+            var result = await _context.Actors.FirstOrDefaultAsync(x => x.Id == id);
+            return result;
+        }
+
+        public void Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Data/Services/IActorService.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce_Application.Models;
+
+namespace ECommerce_Application.Data.Services
+{
+    public interface IActorService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Task<Actor> GetById(int id);
+        Task Add(Actor actor);
+        // check whether id exists in db and if exists then update the database
+        void Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
